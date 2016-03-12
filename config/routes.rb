ba925fcefcbcd4fe0704b1c8cb8938d8290dfcf9
Sean <sean.cox.or@gmail.com>
2016-03-12 21:38:16 +0000

    bundle exec rake test success

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  root 'application#hello'
+  
   get 'static_pages/home'
 
   get 'static_pages/help'
+  
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
+  
   # root 'welcome#index'
 
   # Example of regular route:
