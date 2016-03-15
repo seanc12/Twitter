80d57f16fe5a23edb9f75428c073977f2da29815
Sean <sean.cox.or@gmail.com>
2016-03-15 00:56:58 +0000

    signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   
-  get 'users/new'
-
-  Rails.application.routes.draw do
-  get 'users/new'
-
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  resources :users
+  
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -67,4 +64,3 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
