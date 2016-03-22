fd89e999c674c176733f1bed75ce3925e27899b0
Sean <sean.cox.or@gmail.com>
2016-03-22 04:30:22 +0000

    production test

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -20,18 +20,13 @@ class MicropostsController < ApplicationController
     redirect_to request.referrer || root_url
   end
 
-  private
-    def micropost_params
-      params.require(:micropost).permit(:content, :picture)
-    end
+  def micropost_params
+    params.require(:micropost).permit(:content, :picture)
+  end
 
-    def micropost_params
-      params.require(:micropost).permit(:content)
-    end
-    
-    def correct_user
-      @micropost = current_user.microposts.find_by(id: params[:id])
-      redirect_to root_url if @micropost.nil?
-    end
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
+  end
   
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,13 +5,13 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
-  
-  # Validates the size of an uploaded picture.
+
+  private
+
+    # Validates the size of an uploaded picture.
     def picture_size
       if picture.size > 5.megabytes
         errors.add(:picture, "should be less than 5MB")
       end
     end
 end
-
-
